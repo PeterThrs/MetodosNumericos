b0e36e8b0aed34f7ab92588526a23927259a6d0e
peter <peterlopezthrs@gmail.com>
2023-05-25 17:21:08 -0600

    Listo el programa de Runge-Kutta

--- MetodosNumericoTrabajoFinal/src/main/java/com/metodos/metodosnumericotrabajofinal/RungeKuttaSolver.java
+++ MetodosNumericoTrabajoFinal/src/main/java/com/metodos/metodosnumericotrabajofinal/RungeKuttaSolver.java
@@ -2,68 +2,117 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.metodos.metodosnumericotrabajofinal;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 
 public class RungeKuttaSolver {
+
+    private List<Double> valoresX;
+    private List<Double> valoresY;
+    private List<Double> valoresY1;
+    private int cont;
+
+    public RungeKuttaSolver() {
+        valoresX = new ArrayList<>();
+        valoresY = new ArrayList<>();
+        valoresY1 = new ArrayList<>();
+    }
+
     static class DifferentialEquation {
+
         double x;
         double y;
-        
+
         public DifferentialEquation(double x, double y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    public static double rungeKutta4(Function<DifferentialEquation, Double> f, double x0, double y0, double h, double x) {
+    public double runKutta4(Function<DifferentialEquation, Double> f, double x0, double y0, double h, double x) {
         double yn = y0;
-        double xn = x0;
+        BigDecimal xn = BigDecimal.valueOf(x0);
+        cont = 0;
 
-        while (xn < x) {
-            DifferentialEquation equation = new DifferentialEquation(xn, yn);
-            
-            double k1 = h * f.apply(equation);
-            
-            equation.x = xn + h/2;
-            equation.y = yn + k1/2;
-            double k2 = h * f.apply(equation);
-            
-            equation.y = yn + k2/2;
-            double k3 = h * f.apply(equation);
-            
-            equation.x = xn + h;
-            equation.y = yn + k3;
-            double k4 = h * f.apply(equation);
+        while (xn.doubleValue() < x) {
+            valoresY.add(yn); 
+            //System.out.println("xn = " + xn);
+            DifferentialEquation equation = new DifferentialEquation(xn.doubleValue(), yn);
 
-            yn += (k1 + 2*k2 + 2*k3 + k4) / 6;
-            xn += h;
+            double k1 = f.apply(equation);
+            System.out.println("k1 = " + k1);
+
+            equation.x = xn.doubleValue() + h / 2;
+            equation.y = yn + (k1 * h / 2);
+            double k2 = f.apply(equation);
+            System.out.println("k2 = " + k2);
+
+            equation.y = yn + (k2 * h / 2);
+            double k3 = f.apply(equation);
+            System.out.println("k3 = " + k3);
+
+            equation.x = xn.doubleValue() + h;
+            equation.y = yn + (k3 * h);
+            double k4 = f.apply(equation);
+            System.out.println("k4 = " + k4);
+
+            yn += ((k1 + 2 * k2 + 2 * k3 + k4) * (h)) / 6;
+            valoresY1.add(yn);
+            valoresX.add(xn.doubleValue());
+            //System.out.println("yn = " + yn);
+            xn = xn.add(BigDecimal.valueOf(h));
+            cont++;
+            System.out.println("xn = " + xn);
         }
-
+        graficar();
         return yn;
     }
 
-    public static void main(String[] args) {
-        // Ejemplo de uso
-        // Definimos la función f(x, y) que representa la ecuación diferencial dy/dx = f(x, y)
-        Function<DifferentialEquation, Double> f = equation -> equation.x * equation.y; // Por ejemplo, dy/dx = x * y
+    public void graficar() {
+        System.out.printf("%-10s %-10s %-10s %-10s %n", "Iteracion", "   X   ","   Y   ", "  Yi+1  ");
+        for (int i = 0; i < cont; i++) {
+            System.out.printf("%-10s %-10s %-10s %-10s %n", i, valoresX.get(i),Math.round(valoresY.get(i) * 1000) * 0.001 ,Math.round(valoresY1.get(i) * 1000) * 0.001);
+        }
+        
+//        System.out.println("");
+//        // Crear gráfica
+//        XYChart chart = QuickChart.getChart("Solución de la Ecuación Diferencial", "t", "y", "y(t)", valoresX, valoresY);
+//
+//        // Mostrar la gráfica
+//        new SwingWrapper<>(chart).displayChart();
+    }
 
+    public static void main(String[] args) {
+
+        //Function<DifferentialEquation, Double> funcion = equation -> equation.x * Math.sqrt(equation.y);
+        Function<DifferentialEquation, Double> funcion = equation -> equation.y * Math.pow(equation.x, 3) - 1.1 * equation.y;
+        //Function<DifferentialEquation, Double> funcion = equation -> -2 * Math.pow(equation.x, 3) + 12 * Math.pow(equation.x, 2) - 20 * equation.x + 8.5;
+        
+        //Function<DifferentialEquation, Double> funcion = equation -> 4 * Math.pow(Math.E, 0.8 * equation.x) - 0.5 * equation.y;
+
+        
         // Definimos las condiciones iniciales
         double x0 = 0; // Valor inicial de x
         double y0 = 1; // Valor inicial de y
 
         // Definimos el tamaño del paso
-        double h = 0.1; // Tamaño del paso
+        double h = 0.5; // Tamaño del paso
 
         // Definimos el valor de x para el cual queremos obtener el valor de y
-        double x = 1; // Valor de x para el cual queremos obtener y
+        double x = 2; // Valor de x para el cual queremos obtener y
 
         // Aplicamos el método de Runge-Kutta de cuarto orden
-        double y = rungeKutta4(f, x0, y0, h, x);
+        RungeKuttaSolver rs = new RungeKuttaSolver();
+        double y = rs.runKutta4(funcion, x0, y0, h, x);
 
         // Imprimimos el resultado
-        System.out.println("El valor de y en x = " + x + " es: " + y);
+        //System.out.println("El valor de y en x = " + x + " es: " + y);
     }
 }
