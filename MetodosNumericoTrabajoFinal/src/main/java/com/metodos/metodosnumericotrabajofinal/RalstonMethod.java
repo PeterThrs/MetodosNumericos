b4e4aa0a1a5566bdbe8aafe73fcf1ea4c1a1e1c9
peter <peterlopezthrs@gmail.com>
2023-05-26 00:06:36 -0600

    Trabajando en el metodo Ralston2

--- MetodosNumericoTrabajoFinal/src/main/java/com/metodos/metodosnumericotrabajofinal/RalstonMethod.java
+++ MetodosNumericoTrabajoFinal/src/main/java/com/metodos/metodosnumericotrabajofinal/RalstonMethod.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.metodos.metodosnumericotrabajofinal;
 
@@ -12,14 +12,14 @@ import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 
-public class RungeKutta {
+public class RalstonMethod {
 
     private List<Double> valoresX;
     private List<Double> valoresY;
     private List<Double> valoresY1;
     private int cont;
 
-    public RungeKutta() {
+    public RalstonMethod() {
         valoresX = new ArrayList<>();
         valoresY = new ArrayList<>();
         valoresY1 = new ArrayList<>();
@@ -36,46 +36,45 @@ public class RungeKutta {
         }
     }
 
-    public double runKutta4(Function<DifferentialEquation, Double> f, double x0, double y0, double h, double x) {
+    public double runRalston(Function<DifferentialEquation, Double> f, double x0, double y0, double h, double x) {
         double yn = y0;
-        BigDecimal yn_1 = BigDecimal.valueOf(0); 
         BigDecimal xn = BigDecimal.valueOf(x0);
+        BigDecimal yn_1 = BigDecimal.valueOf(0); 
+        BigDecimal xn_1 = new BigDecimal(0); 
         cont = 0;
 
-        while (xn.doubleValue() < x) {
+        while (xn_1.doubleValue() < x) {
+            valoresY.add(yn); 
             
+            System.out.println("\n______Interacion: " + cont);
+            System.out.println("xn = " + xn);
+            System.out.println("yn = " + yn);
             
-            //System.out.println("xn = " + xn);
             DifferentialEquation equation = new DifferentialEquation(xn.doubleValue(), yn);
 
             double k1 = f.apply(equation);
             System.out.println("k1 = " + k1);
 
-            equation.x = xn.doubleValue() + h / 2;
-            equation.y = yn + (k1 * h / 2);
+            equation.x = xn.doubleValue() +  ((3 / 4) * h);
+            System.out.println("xn.doubleValue() +  ((3.0 / 4.0) * h = " + (xn.doubleValue() +  ((3.0 / 4.0) * h)));
+            equation.y = yn + (3.0 / 4.0) * h * k1;
+            System.out.println("yn + (3.0 / 4.0) * h * k1 = " + yn + (3.0 / 4.0) * h * k1);
             double k2 = f.apply(equation);
             System.out.println("k2 = " + k2);
 
-            equation.y = yn + (k2 * h / 2);
-            double k3 = f.apply(equation);
-            System.out.println("k3 = " + k3);
-
-            equation.x = xn.doubleValue() + h;
-            equation.y = yn + (k3 * h);
-            double k4 = f.apply(equation);
-            System.out.println("k4 = " + k4);
-
-            yn_1 = new BigDecimal(yn + ((k1 + 2 * k2 + 2 * k3 + k4) * (h)) / 6);
+            yn_1 = new BigDecimal(yn * (1.0 / 3.0) * h * k1 + (2.0 / 3.0) * h * k2);
+            xn_1 = xn.add(BigDecimal.valueOf(h));
             
-            valoresY.add(yn); 
-            valoresY1.add(yn_1.doubleValue()); 
-            valoresX.add(xn.doubleValue());
+            xn = xn_1; 
+            yn = yn_1.doubleValue(); 
             
-            xn = xn.add(BigDecimal.valueOf(h));
-            
-            yn = yn_1.doubleValue();
-            cont++;
+            System.out.println("yn = " + yn);
             System.out.println("xn = " + xn);
+            
+            valoresY1.add(yn_1.doubleValue());
+            valoresX.add(xn_1.doubleValue());
+            cont++;
+            
         }
         graficar();
         return yn;
@@ -84,7 +83,7 @@ public class RungeKutta {
     public void graficar() {
         System.out.printf("%-10s %-10s %-10s %-10s %n", "Iteracion", "   X   ","   Y   ", "  Yi+1  ");
         for (int i = 0; i < cont; i++) {
-            System.out.printf("%-10s %-10s %-10s %-10s %n", i, valoresX.get(i),Math.round(valoresY.get(i) * 1000) * 0.001 ,Math.round(valoresY1.get(i) * 1000) * 0.001);
+            System.out.printf("%-10s %-10s %-10s %-10s %n", i, valoresX.get(i),Math.round(valoresY.get(i) * 1000) * 0.001 ,valoresY1.get(i));
         }
         
 //        System.out.println("");
@@ -97,9 +96,7 @@ public class RungeKutta {
 
     public static void main(String[] args) {
 
-        
-          Function<DifferentialEquation, Double> funcion = equation -> equation.x * Math.sqrt(equation.y);
-        //Function<RungeKutta.DifferentialEquation, Double> funcion = equation -> -2 * Math.pow(equation.x, 3) + 12 * Math.pow(equation.x, 2) - 20 * equation.x + 8.5; 
+        Function<DifferentialEquation, Double> funcion = equation -> equation.x * Math.sqrt(equation.y);
         //Function<DifferentialEquation, Double> funcion = equation -> Math.pow(equation.x, 2) + Math.pow(equation.y, 2); 
         //Function<DifferentialEquation, Double> funcion = equation -> equation.y * Math.pow(equation.x, 3) - 1.1 * equation.y;
         //Function<DifferentialEquation, Double> funcion = equation -> -2 * Math.pow(equation.x, 3) + 12 * Math.pow(equation.x, 2) - 20 * equation.x + 8.5;
@@ -117,9 +114,9 @@ public class RungeKutta {
         // Definimos el valor de x para el cual queremos obtener el valor de y
         double x = 1.6; // Valor de x para el cual queremos obtener y
 
-        // Aplicamos el método de Runge-Kutta de cuarto orden
-        RungeKutta rs = new RungeKutta();
-        double y = rs.runKutta4(funcion, x0, y0, h, x);
+        // Aplicamos el método de Ralston
+        RalstonMethod rs = new RalstonMethod();
+        double y = rs.runRalston(funcion, x0, y0, h, x);
 
         // Imprimimos el resultado
         //System.out.println("El valor de y en x = " + x + " es: " + y);
